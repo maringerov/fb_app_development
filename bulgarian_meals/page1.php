747afbf2d4ba0f97db5df48b33fd13fe3c839293
Marin Gerov <marin.gerov@gmail.com>
2014-04-15 06:45:36 +0300

    Modified page1.php to include form for submitting a recipe

--- bulgarian_meals/page1.php
+++ bulgarian_meals/page1.php
@@ -9,3 +9,28 @@ if ($facebook) {
 ?>
 <div><?=$_REQUEST["content"];?></div>
 <a href='index.php'>Go back to homepage</a>
+<div class="row">
+  <div class="col-md-12">
+    <form role="form">
+      <div class="form-group">
+        <label for="recipeName">Recipe's Name</label>
+        <input type="text" class="form-control" id="recipeName" placeholder="Enter your recipe's name">
+      </div>
+      <div class="form-group">
+        <label for="fullRecipe">Your Recipe</label>
+        <textarea class="form-control" rows="5" id="fullRecipe"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="uploadImg">Upload Image</label>
+        <input type="file" id="uploadImg">
+        <p class="help-block">Image should be at least 810X500px</p>
+      </div>
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> I agree with the terms and conditions
+        </label>
+      </div>
+      <button type="submit" class="btn btn-default" value="post">Upload Your Recipe</button>
+    </form>
+  </div>
+</div>
